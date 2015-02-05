5ddaea059e09b3d72114b2b0b8eadd7814f13912
mockturtl <mockturtl@gmail.com>
2015-02-05 18:01:46 -0500

    cleanup

--- lib/dotenv.dart
+++ lib/dotenv.dart
@@ -3,50 +3,37 @@ library dotenv;
 import 'package:logging/logging.dart';
 import 'dart:io';
 
+part 'src/parser.dart';
+
+var _env = new Map.from(Platform.environment);
+
 /// A copy of [Platform.environment] including variables loaded at runtime from a file.
-Map<String, String> env = new Map.from(Platform.environment);
+Map<String, String> get env => _env;
 
 final _log = new Logger('dotenv');
 final _pkgroot = Platform.script.resolve('..');
+
+const _psr = const Parser();
+
+/// Overwrite [_env] with a clean [Platform.environment].  Useful for testing.
+Map clean() => _env = new Map.from(Platform.environment);
+
+/// True if all supplied variables are present in the environment; false otherwise.
+bool isEveryDefined(Iterable<String> vars) => vars.every(_defined);
 
 /// Read environment variables from [filename] and add them to [env].
 void load([String filename = '.env']) {
   var f = new File.fromUri(_pkgroot.resolve(filename));
   _verify(f);
-
   var lines = f.readAsLinesSync();
-  _verifyLineFormat(lines);
-
-  var _vars = {};
-  lines.forEach((line) {
-    var substrs = line.split('=');
-    var rawK = substrs[0];
-    var k = rawK.replaceAll('export', '').trim(); // omit 'export' keyword
-    var rawV = substrs[1];
-    var v = rawV.replaceAll(new RegExp(r'"'), '') // double-quoted values
-        .replaceAll(new RegExp(r"'"), '') // single-quoted values
-        .replaceAll(new RegExp(r'#.*$'), '') // comments
-        .trim(); // TODO: variable substitution
-    _vars[k] = v;
-  });
-
-  _vars.forEach((k, v) {
-    _log.finer('load: $k=$v');
-  });
-
-  env.addAll(_vars);
+  _env.addAll(_psr.parse(lines));
 }
 
-void _verifyLineFormat(Iterable lines) {
-  if (!lines.every((String l) => l.contains('='))) {
-    _log.severe('Missing "="; aborting');
-    exit(13);
-  }
-}
+bool _defined(String k) => _env[k] != null && _env[k].isNotEmpty;
 
 void _verify(File f) {
   if (!f.existsSync()) {
-    _log.severe('Cannot find "$f"; aborting.');
-    exit(11);
+    _log.severe('_verify: Cannot find file "$f".');
+    exitCode = 1;
   }
 }
---
+++ lib/src/parser.dart
@@ -0,0 +1,40 @@
+part of dotenv;
+
+class Parser {
+  static final _log = new Logger('Parser');
+
+  const Parser();
+
+  Map<String, String> parse(List<String> lines) {
+    if (!_validate(lines)) {
+      _log.severe('parse: validation failed; aborting');
+      exitCode = 1;
+      return {};
+    }
+
+    var out = {};
+    lines.forEach((line) {
+      var substrs = line.split('=');
+      var rawK = substrs[0];
+      var k = rawK.replaceAll('export', '').trim(); // omit 'export' keyword
+      var rawV = substrs[1];
+      var v = rawV.replaceAll(new RegExp(r'"'), '') // double-quoted values
+          .replaceAll(new RegExp(r"'"), '') // single-quoted values
+          .replaceAll(new RegExp(r'#.*$'), '') // comments
+          .trim(); // TODO: variable substitution
+      out[k] = v;
+    });
+    out.forEach((k, v) {
+      _log.finer('load: $k=$v');
+    });
+    return out;
+  }
+
+  bool _validate(Iterable lines) {
+    if (lines.any((String l) => !l.contains('='))) {
+      _log.severe('_validate: missing "="');
+      return false;
+    }
+    return true;
+  }
+}
