3ff9a29604abbd4e8c0ef3da3429dd82fdda76d5
mockturtl <mockturtl@gmail.com>
2015-05-12 18:16:39 -0400

    cleanup

--- example/example.dart
+++ example/example.dart
@@ -1,25 +1,13 @@
+import 'dart:io';
+
 import 'package:dotenv/dotenv.dart' as dotenv;
-import 'package:logging/logging.dart';
-
-get env => dotenv.env;
-
-final log = new Logger('example');
 
 void main() {
-  _config();
-
   dotenv.load();
-  log.info('main: value of foo is ${env['foo']}');
-  log.info('main: value of baz is ${env['baz']}');
-  log.info('main: your home directory is: ${env['HOME']}');
+
+  stdout.writeln('main: value of foo is ${env['foo']}');
+  stdout.writeln('main: value of baz is ${env['baz']}');
+  stdout.writeln('main: your home directory is: ${env['HOME']}');
 }
 
-void _config() {
-  Logger.root.level = Level.FINE;
-  Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name} ${_toMsg(rec)}');
-  });
-}
-
-String _toMsg(LogRecord rec) =>
-    '${rec.time}: [${rec.loggerName}] ${rec.message}';
+get env => dotenv.env;
