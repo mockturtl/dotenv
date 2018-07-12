b026779f2c1ca814b2ab4aedc73ce0364cf8fa9c
Tobe O <thosakwe@gmail.com>
2018-07-12 12:22:11 -0400

    Apply dartfmt

--- bin/dotenv.dart
+++ bin/dotenv.dart
@@ -8,7 +8,8 @@ final _argPsr = new ArgParser()
   ..addOption('file',
       abbr: 'f',
       defaultsTo: '.env',
-      help: 'File to read.\nProvides environment variable definitions, one per line.');
+      help:
+          'File to read.\nProvides environment variable definitions, one per line.');
 
 /// Prints the [env] map.
 ///
